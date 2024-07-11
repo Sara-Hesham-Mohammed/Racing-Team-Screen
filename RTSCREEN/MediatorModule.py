bd2246a81a1d25fe6eccd5ade6eb8562c85fe8d4
sarahsspace <sarah224276@bue.edu.eg>
2024-07-11 14:11:58 +0300

    get calc reading func
    added try exceptions and debugging

--- RTSCREEN/MediatorModule.py
+++ RTSCREEN/MediatorModule.py
@@ -49,12 +49,12 @@ class Mediator:
 
     #####################################################################################
     def getCalculatedReading(self,sensorName):
-        speed = self.arduino.calcSpeed()
+        '''speed = self.arduino.calcSpeed()
         distanceTravelled = self.arduino.getDistanceTravelled()
         current = self.arduino.getCurrent()
         voltage = self.arduino.getVoltage()
         batteryPercentage = self.arduino.getBatteryPercentage()
-
+       
         switch = {
             "speed": speed,
             "distanceTravelled": distanceTravelled,
@@ -64,7 +64,53 @@ class Mediator:
         }
         print(f"SWITCH CASE{switch[sensorName]}")
 
-        return switch[sensorName]
+        return switch[sensorName]'''
+        
+        sensorReadings = {}
+
+        try: 
+            speed = self.arduino.calcSpeed()
+            if speed is not None:
+                sensorReadings["speed"] = speed
+        except Exception as e:
+                    print(f"Error in getting speed: {e}")
+
+        try: 
+            distanceTravelled = self.arduino.getDistanceTravelled()
+            if distanceTravelled is not None:
+                sensorReadings["distanceTravelled"] = distanceTravelled
+        except Exception as e:
+                    print(f"Error in getting distance travelled: {e}")
+
+        try: 
+            voltage = self.arduino.getVoltage()
+            if voltage is not None:
+                sensorReadings["voltage"] = voltage
+        except Exception as e:
+                    print(f"Error in getting voltage: {e}")
+
+        try: 
+            current = self.arduino.getCurrent()
+            if current is not None:
+                sensorReadings["current"] = current
+        except Exception as e:
+                    print(f"Error in getting current: {e}")
+
+        try: 
+            batteryPercentage = self.arduino.getBatteryPercentage()
+            if batteryPercentage is not None:
+                sensorReadings["batteryPercentage"] = batteryPercentage
+        except Exception as e:
+                    print(f"Error in getting batterypercentage: {e}")
+
+        print(f"Available sensor names: {sensorReadings.keys()}") #for debugging 
+        #returns the ones retrieved by arduino object excluding ones with none
+        print(f"SWITCH CASE: {sensorReadings[sensorName]}")
+        return sensorReadings[sensorName]
+
+
+
+        
 
 
 
